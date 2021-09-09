daab6eb2431889c72f9c8f6840b1cae366a51a7c
naviak <m.dudar@g.nsu.ru>
2021-09-09 14:58:33 +0300

    Update FloatingPoint.scala
    eps Array func added

--- Task1/FloatingPoint.scala
+++ Task1/FloatingPoint.scala
@@ -2,10 +2,11 @@ package Task1
 
 import scala.math.Fractional.Implicits.infixFractionalOps
 import scala.math.Ordered.orderingToOrdered
+import scala.reflect.ClassTag
 
 object FloatingPoint extends App{
 
-  class FloatNumber[F](oneInSystem: F,base: F)(implicit ev: Fractional[F]) {
+  class FloatNumber[F:ClassTag](oneInSystem: F)(implicit ev: Fractional[F]) {
     private val one:F = oneInSystem
     private val two:F = ev.plus(one,one)
     def epsilon(): F = {
@@ -34,20 +35,23 @@ object FloatingPoint extends App{
       val ls = s.takeWhile(_ != one).toList
       ls.length
     }
+
+    def epsArray(): Array[F]  = {
+      val eps = epsilon()
+      Array(one,one + eps/two,one + eps,one + eps + eps/two)
+    }
   }
 
-  val floatNumber = new FloatNumber(1.0f,10.0f)
-  val doubleNumber = new FloatNumber(1.0,10.0)
-
-  println(s"epsilon for Float:\t ${floatNumber.epsilon}")
+  val floatNumber = new FloatNumber(1.0f)
+  val doubleNumber = new FloatNumber(1.0)
+  println(s"epsil on for Float:\t ${floatNumber.epsilon}")
   println(s"min exponent of Float:\t ${floatNumber.minExponent}")
   println(s"max exponent of Float:\t ${floatNumber.maxExponent}")
   println(s"mantissa of Float calculated by using lazy:\t ${floatNumber.lazyNumMantiss}")
-
-  println("")
-
+  println(floatNumber.epsArray.mkString(" "))
   println(s"epsilon for Double:\t ${doubleNumber.epsilon}")
   println(s"min exponent of Double:\t ${doubleNumber.minExponent}")
   println(s"max exponent of Double:\t ${doubleNumber.maxExponent}")
   println(s"mantissa of Double calculated by using lazy:\t ${doubleNumber.lazyNumMantiss}")
+  println(doubleNumber.epsArray.mkString(" "))
 }
