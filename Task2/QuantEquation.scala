4f4adcbed152f03e781720b1f966b0702dc8054e
naviak <m.dudar@g.nsu.ru>
2021-09-17 10:08:34 +0300

    Update QuantEquation.scala

--- Task2/QuantEquation.scala
+++ Task2/QuantEquation.scala
@@ -6,38 +6,39 @@ import breeze.optimize._
 object QuantEquation {
   object ChrestomaticEquation {
     val a = 10
-    private val U0 = 10
+    private val U0 = 100
     private val fig = Figure()
     val p = fig.subplot(0)
 
     val razr = endOfGraph
     val start = razr
-    val end = -U0
+    val end = U0
 
     private def endOfGraph(): Double = {
-      val razr = (Math.pow(Math.PI, 2) / (2 * a * a * U0) - 1) * U0
-      razr
+      val razr = (1 - Math.pow(Math.PI, 2) / (2 * a * a * U0))*U0
+      if(razr < 0) 0
+      else razr
     }
 
     def ctgArg(x: Double): Double = {
-      val ctgSqrtArg = 2 * a * a * U0 * (1 + x / U0)
+      val ctgSqrtArg = 2 * a * a * U0 * (1 - x / U0)
       math.sqrt(ctgSqrtArg)
     }
 
     def f(x: Double) = {
       val ctg = 1 / math.tan(ctgArg(x))
-      val rightArg = -U0 / x - 1
+      val rightArg = U0 / x - 1
       val right = math.sqrt(rightArg)
       ctg - right
     }
 
     def plotF[T <: Method](method: T): Unit = {
       println(endOfGraph())
-      val x = linspace(-U0, 0, 10000)
+      val x = linspace(0, U0, 10000)
       val y = x.map {
         case x => val t = f(x)
           if (t.isNaN) 0
-          else if (t.isInfinite) 1e4
+          else if (t.isInfinite || t > 1000 ) 1e2
           else t
       }
       p += plot(x, y)
@@ -54,7 +55,7 @@ object QuantEquation {
     val newton = new Newton()
     val iter = new SimpleIterations()
     val dich = new Dichotomy()
-    ChrestomaticEquation.plotF(dich)
+    ChrestomaticEquation.plotF(newton)
   }
 
 }
