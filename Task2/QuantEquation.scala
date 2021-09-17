eee6d6f3f178f17ec9f61de11d9809e2566f933e
naviak <m.dudar@g.nsu.ru>
2021-09-17 09:51:13 +0300

    Update QuantEquation.scala

--- Task2/QuantEquation.scala
+++ Task2/QuantEquation.scala
@@ -6,23 +6,34 @@ import breeze.optimize._
 object QuantEquation {
   object ChrestomaticEquation {
     val a = 10
-    private val U0 = 1000
+    private val U0 = 10
     private val fig = Figure()
-    val start = -3
-    val end = -5
     val p = fig.subplot(0)
 
-    def f(x: Double) = {
+    val razr = endOfGraph
+    val start = razr
+    val end = -U0
+
+    private def endOfGraph(): Double = {
+      val razr = (Math.pow(Math.PI, 2) / (2 * a * a * U0) - 1) * U0
+      razr
+    }
+
+    def ctgArg(x: Double): Double = {
       val ctgSqrtArg = 2 * a * a * U0 * (1 + x / U0)
-      val ctgArg = math.sqrt(ctgSqrtArg)
-      val ctg = 1 / math.tan(ctgArg)
+      math.sqrt(ctgSqrtArg)
+    }
+
+    def f(x: Double) = {
+      val ctg = 1 / math.tan(ctgArg(x))
       val rightArg = -U0 / x - 1
       val right = math.sqrt(rightArg)
       ctg - right
     }
 
     def plotF[T <: Method](method: T): Unit = {
-      val x = linspace(-1000, 0, 1000000)
+      println(endOfGraph())
+      val x = linspace(-U0, 0, 10000)
       val y = x.map {
         case x => val t = f(x)
           if (t.isNaN) 0
@@ -30,9 +41,8 @@ object QuantEquation {
           else t
       }
       p += plot(x, y)
-
-      val sol = method.solve(-4.7, f, 1e-10)
-      p += plot(DenseVector.fill(1000)(sol), linspace(-120000, 120000, 1000), '.')
+      val sol = method.solve((start + end) / 2, f, 1e-10)
+      p += plot(DenseVector.fill(1000)(sol), linspace(-120, 120, 1000), '.')
       println(f(sol))
       p.xlabel = "x axis"
       p.ylabel = "y axis"
@@ -44,7 +54,7 @@ object QuantEquation {
     val newton = new Newton()
     val iter = new SimpleIterations()
     val dich = new Dichotomy()
-    ChrestomaticEquation.plotF(iter)
+    ChrestomaticEquation.plotF(dich)
   }
 
 }
