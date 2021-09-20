c38a93e3905a76e0f37395d41b912167ecab4e15
naviak <m.dudar@g.nsu.ru>
2021-09-20 17:05:40 +0300

    helper funcs

--- Task3/IntegralSolver.scala
+++ Task3/IntegralSolver.scala
@@ -5,11 +5,28 @@ import breeze.plot._
 import breeze.optimize._
 
 object IntegralSolver extends App{
-  val ls = linspace(0.0, math.Pi,1000).toArray.toList
-  val trap = Trapezoidal(math.sin)
-  println(trap.calculate(ls))
+  object Helper{
 
+    private val x = linspace(0.0, 3 * math.Pi,6)
+
+    def integrate[T <: Solver](method: T) = {
+      val ls = x.toArray.toList
+      method.calculate(ls)
+    }
+
+    def plotIntegral[T <: Solver](method: T): Unit = {
+      val fig = Figure()
+      val p = fig.subplot(0)
+      p += plot(x,x.map(t => method.f(t)))
+
+      fig.refresh
+    }
+  }
+  val trap = Trapezoidal(math.sin)
+  println(Helper.integrate(trap))
+  Helper.plotIntegral(trap)
   val simp = Simpson(math.sin)
-  println(simp.calculate(ls))
+  println(Helper.integrate(simp))
+
 
 }
--- Task3/Solver.scala
+++ Task3/Solver.scala
@@ -1,5 +1,6 @@
 package Integral
 trait Solver{
+  def f: Double => Double
   def calculate(ar: List[Double]): Double = {
     val arr = ar.zip(ar.tail)
     arr.foldLeft(0.0){ case (acc, x1) => acc + method(x1._1, x1._2)}
