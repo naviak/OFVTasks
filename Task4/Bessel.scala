a004e88a33b2f3759146dd839c283ff07c560d9d
naviak <m.dudar@g.nsu.ru>
2021-09-29 09:58:52 +0300

    lightweight

--- Task4/Bessel.scala
+++ Task4/Bessel.scala
@@ -1,23 +1,48 @@
-package Integral
+package Bessel
 
-import breeze.linalg.{DenseVector, linspace}
+import breeze.linalg.linspace
+
 
 object Bessel extends App {
-  def BesselF(x: Double, m: Int): Double = {
-    val func = BesselFunction(x, m)
-    val solver = Simpson(func)
-    solver.calculate() / math.Pi
+  trait Solver {
+    def function: Double => Double
+
+    val ar: List[Double]
+
+    def calculate(): Double = {
+      val arr = ar.zip(ar.tail)
+      arr.foldLeft(0.0) { case (acc, x1) => acc + method(x1._1, x1._2) }
+    }
+
+    protected def method(x1: Double, x2: Double): Double
   }
 
-  def BesselDerivative(x: Double, m: Int): Double = {
-    val derivativeStep: Double = 10e-6
-    (BesselF(x + derivativeStep, m) - BesselF(x - derivativeStep, m)) / (2.0 * derivativeStep)
+  case class Simpson(function: Double => Double, ar: List[Double]) extends Solver {
+    override def method(x1: Double, x2: Double): Double = {
+      ((x2 - x1) / 6.0) * (function(x1) + 4.0 * function((x1 + x2) / 2.0) + function(x2))
+    }
   }
 
-  var x: Double = 0.1
-  while (x < 1.1) {
+  def besselFunction(x: Double, m: Int): Double = {
+    def func(t: Double): Double = {
+      math.cos(m * t - x * math.sin(t))
+    }
+
+    val xval = linspace(0, math.Pi, 500).toArray.toList
+    val calc = Simpson(func, xval)
+    calc.calculate() / math.Pi
+  }
+
+  def besselFunctionDerivative(x: Double, m: Int) = {
+    val derivativeStep = 10e-6
+    (besselFunction(x + derivativeStep, m) - besselFunction(x - derivativeStep, m)) / (2.0 * derivativeStep)
+  }
+
+  var x = 0.0
+  while (x < math.Pi) {
     println(s"x = $x")
-    val result: Double = BesselDerivative(x, 0) + BesselF(x, 1)
+    val result: Double = besselFunctionDerivative(x, 0) + besselFunction(x, 1)
+    require(result < 1e-10)
     println(result)
     x += 0.1
   }
--- Task4/Simpson.scala
+++
@@ -1,7 +0,0 @@
-package Integral
-
-case class Simpson[T <: Function](function: T) extends Solver {
-  override def method(x1: Double, x2: Double): Double = {
-    (x2 - x1) / 6.0 * (function.f(x1) + 4.0 * function.f((x1 + x2) / 2.0) + function.f(x1))
-  }
-}
