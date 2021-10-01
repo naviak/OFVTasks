b64141aee269d792c04d9c1a59a84403e64b7ca6
naviak <m.dudar@g.nsu.ru>
2021-10-01 14:01:02 +0300

    Update Bessel.scala

--- Task4/Bessel.scala
+++ Task4/Bessel.scala
@@ -34,23 +34,41 @@ object Bessel extends App {
     calc.calculate() / math.Pi
   }
 
-  def besselFunctionDerivative(x: Double, m: Int) = {
+  def besselFunctionDerivative(x: Double, m: Int): Double = {
     val derivativeStep = 10e-6
-    (besselFunction(x + derivativeStep, m) - besselFunction(x - derivativeStep, m)) / (2.0 * derivativeStep)
+    if(x == 0.0){
+      val r = (besselFunction(x + derivativeStep, m) - besselFunction(x, m)) / derivativeStep
+      r
+    }
+    if (x == math.Pi){
+      val r = (besselFunction(x, m) - besselFunction(x - derivativeStep, m)) / derivativeStep
+      r
+    }
+    else {
+      val r = (besselFunction(x + derivativeStep, m) - besselFunction(x - derivativeStep, m)) / (2.0 * derivativeStep)
+      r
+    }
   }
 
   var x = 0.0
+  var ls:List[Double] = List()
   while (x < math.Pi) {
     println(s"x = $x")
     val result: Double = besselFunctionDerivative(x, 0) + besselFunction(x, 1)
+    ls = ls :+ result
     require(result < 1e-10)
     println(result)
     x += 0.1
   }
   val fig = Figure()
+  val fig2 = Figure()
   val p = fig.subplot(0)
+  val p2 = fig2.subplot(0)
   val xx = linspace(0,20,4000)
   p += plot(xx, xx.map(t => besselFunction(t,0)))
   p += plot(xx,xx.map(t => besselFunction(t,1)))
+  p2 += plot(linspace(0.0,math.Pi,ls.length),ls)
+  p2.setYAxisDecimalTickUnits()
   fig.refresh
+  fig2.refresh
 }
